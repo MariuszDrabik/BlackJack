6cf77b8af0db23446a40df4f3ee2596149ab6a45
Mariusz Drabik <drabik.mariusz@gmail.com>
2022-03-21 11:37:25 +0100

    split classes to files game work

--- blackjack_test.py
+++ blackjack_test.py
@@ -1,4 +1,5 @@
-from blackjack import Card, Deck
+from cards import Card, Deck
+from player import Player
 
 COLOR = ('♥', '♦', '♠', '♣')
 
@@ -6,14 +7,20 @@ COLOR = ('♥', '♦', '♠', '♣')
 def test_card():
     king = Card('♥', 'KING', 10)
     ten = Card('♥', '8', 8)
-    
-    assert king != ten
-    assert king > ten
-    assert ten < king
+
+    assert king.value == 10
+    assert ten.value == 8
 
 
 def test_deck():
     deck = Deck()
-    
+
     assert len(deck) == 52
-    
+
+
+def test_player():
+    diler = Player()
+    user = Player('User')
+
+    assert diler.name == 'Croupier'
+    assert user.name == 'User'
